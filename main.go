cff9c87a6cacfb7107b9a0549be952523c914254
Sayyidusy syauqi Al Ghiffari <sijeding15@gmail.com>
2024-12-22 12:16:01 +0700

    tampil daftar demand bahan bakar

--- main.go
+++ main.go
@@ -1,7 +1,35 @@
-package main 
+package main
 
 import "fmt"
 
-func main (){
-	fmt.Println("Hello, world!")
+type FuelDemand struct {
+	Region string
+	Demand float64
+}
+
+// Data statis demand BBM
+var fuelDemands = []FuelDemand{
+	{"Jakarta", 10000},
+	{"Surabaya", 8500},
+	{"Bandung", 7200},
+	{"Semarang", 6800},
+	{"Medan", 6400},
+	{"Makassar", 5900},
+	{"Palembang", 5700},
+}
+
+// function to print the array
+func printData(data []FuelDemand) {
+	fmt.Println("\nDaftar Demand Bahan Bakar:")
+	fmt.Println("==========================")
+	for i, item := range data {
+		fmt.Printf("%d. Region: %s, Demand: %.f\n", i+1, item.Region, item.Demand)
+	}
+	fmt.Println("==========================")
+}
+
+func main() {
+	fmt.Println("Program Quick Sort Demand Bahan Bakar")
+	fmt.Println("Data Awal:")
+	printData(fuelDemands)
 }
